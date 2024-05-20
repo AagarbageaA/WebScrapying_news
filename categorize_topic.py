81aa622afa1c8ed017c1c249542e225b330ebc6a
gonjo9 <william921101@gmail.com>
2024-05-21 00:17:43 +0800

    add: more tries to change the result of the clustered topics

--- categorize_topic.py
+++ categorize_topic.py
@@ -35,11 +35,11 @@ def categorize(words,stopwords): #利用模型進行主題分類
     # 向量化模型參數設定
     vectorizer_model = TfidfVectorizer(
         encoding="UTF-8",
-        min_df=0.3, # 用於過濾掉在少於此閾值的文檔中出現的詞彙
+        min_df=5, # 用於過濾掉在少於此閾值的文檔中出現的詞彙
         tokenizer=lambda text: text.split(),
         max_features=5000,
         stop_words=list(stopwords)
-        ) 
+    ) 
     
     # BERTopic參數設定
     topic_model = BERTopic(language="chinese (traditional)",
@@ -47,17 +47,21 @@ def categorize(words,stopwords): #利用模型進行主題分類
                         vectorizer_model=vectorizer_model,
                         #nr_topics=20,
                         min_topic_size=2, # 主題最小文檔數
-                        zeroshot_min_similarity=0.35 # 最小相似度
-                        )
+                        zeroshot_min_similarity=0.5 # 最小相似度
+    )
 
-    topic_model.fit_transform(words)
+    topics, probs = topic_model.fit_transform(words)
     topic_list = pd.DataFrame(topic_model.get_topic_info())
     topic_list.to_excel("repo/topic_list.xlsx", index=False)
+
+    df = pd.DataFrame({"topic": topics, "docs": words})
+    df.to_excel("repo/article_topic.xlsx", index=False)
  
 if __name__ == "__main__":
     #讀取新聞內容
     contents = pd.read_excel("repo/news_data.xlsx", engine='openpyxl', sheet_name='Sheet1')
-    with open("repo/stop_words.txt","r") as record: #讀取上次更新的日期
-        stopwords=record.read()
-    words=cut_content(contents)
-    categorize(words,stopwords)
+    with open("repo/stop_words.txt", "r", encoding="utf-8") as record: #讀取上次更新的日期
+        stopwords = record.read()
+    words = cut_content(contents)
+    for i in range(1):
+        categorize(words, stopwords)
